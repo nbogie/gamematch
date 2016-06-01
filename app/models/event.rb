52017914567945933a4f3e8a07d03fd96ddb90e9
Neill <neillzero@gmail.com>
2016-06-01 22:12:44 +0000

    Event.add_known_users dev convenience

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,4 +11,14 @@ class Event < ActiveRecord::Base
   #The field "meetup_event_id" can be either numeric or alphanumeric, and this 
   #may differ from the one used in the url.
   #We save whatever one is given in the id field.
+  
+  def add_known_users
+    ['Maiacetus', 'enz0', 'jonpurkis', 'stereoscopy', 'NormandyWept', 'CDrust'].each do |username|
+      p = Player.find_by_bgg_username username
+      if (!players.member? p)
+        players.push(p)
+      end
+    end
+  end
+  
 end
