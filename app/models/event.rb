6c37ab2a3012c4a786cc9de18a8f572acd17c4f1
Neill <neillzero@gmail.com>
2016-06-14 22:58:06 +0000

    docs.  and a couple links to guild

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,15 +4,15 @@ class Event < ActiveRecord::Base
   has_many :players, :through => :rsvps
 
 
+  #The field "meetup_event_id" can be either numeric or alphanumeric, and this 
+  #may differ from the one used in the url.
+  #We save whatever one is given in the id field.
   def url
     return "https://www.meetup.com/LondonOnBoard/events/#{meetup_event_id}"
   end
 
-  #The field "meetup_event_id" can be either numeric or alphanumeric, and this 
-  #may differ from the one used in the url.
-  #We save whatever one is given in the id field.
-  
   def add_all_members
+    #TAG:devonly
     Player.where('bgg_username is not null').each do |p|
       if (!players.member? p)
         players.push(p)
